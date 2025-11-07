a59807b97f75b589c1c27c38845d352d7488532b
isa_bel <bel494621@gmail.com>
2025-11-06 21:02:24 -0400

    commit

--- WebAPI1/obj/Debug/net8.0/WebAPI1.AssemblyInfo.cs
+++ WebAPI1/obj/Debug/net8.0/WebAPI1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e1e8504f4cd9b1d1888286a333629cc232e750f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
